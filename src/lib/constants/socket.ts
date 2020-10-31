3d4624a7b8039707868b6cf6cd63c5b6ca882a87
Brandon Paiz <paiz.brandon@gmail.com>
2020-10-31 15:04:14 -0700

    Synchronize party's videos

--- src/lib/constants/socket.ts
+++ src/lib/constants/socket.ts
@@ -3,14 +3,17 @@ export enum SocketEvents {
   SEND_MESSAGE = 'send_message',
   CONNECT = 'connect',
   CREATE_PARTY = 'create_party',
-
-  VIDEO_PLAY = 'video_play',
-  VIDEO_PAUSE = 'video_pause',
-  VIDEO_SEEKED = 'video_seeked',
-  VIDEO_PROGRESS = 'video_progress',
   JOIN_PARTY = 'join_party',
+  VIDEO_EVENT = 'video_event',
 }
 
 export interface CreatePartyResponse {
   roomId: string;
+}
+
+export enum VideoSocketEvents {
+  VIDEO_PLAY = 'video_play',
+  VIDEO_PAUSE = 'video_pause',
+  VIDEO_SEEKED = 'video_seeked',
+  VIDEO_PROGRESS = 'video_progress',
 }
---
+++ src/lib/constants/video.ts
@@ -0,0 +1,1 @@
+export const MAX_DESYNC_SEC = 3;
