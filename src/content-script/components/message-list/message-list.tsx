e84d6526baa9384b208b1950282b32c27ba9ee80
stnichol <stnichol@ucsc.edu>
2020-11-29 23:45:04 -0800

    Cleaned for prettier

--- src/content-script/components/message-list/message-list.tsx
+++ src/content-script/components/message-list/message-list.tsx
@@ -27,7 +27,7 @@ const MessageList: React.FC<ReduxProps> = ({ messages }) => {
 
   const isInViewport = (elem: HTMLDivElement | null) => {
     const bounds = elem?.getBoundingClientRect();
-    if(!bounds) return true;
+    if (!bounds) return true;
     return (
       bounds.top >= 0 &&
       // using a magic number of 130
@@ -38,25 +38,24 @@ const MessageList: React.FC<ReduxProps> = ({ messages }) => {
   useEffect(() => {
     const lastMessageId = messages[messages.length - 1]?.id;
     if (lastMessageId && isInViewport(messageRefs.current[lastMessageId])) {
-      console.log(isInViewport(messageRefs.current[lastMessageId]))
       messageRefs.current[lastMessageId]?.scrollIntoView(false);
     }
   }, [messages]);
 
   return (
     <Container>
-        {messages?.map(({ content, isOwnMessage, user, id }) => (
-          <MessageCell
-            message={content}
-            isOwnMessage={isOwnMessage}
-            userName={user.name}
-            avatarUrl={user.avatarUrl}
-            key={id}
-            ref={ref => {
-              messageRefs.current[id] = ref;
-            }}
-          />
-        ))}
+      {messages?.map(({ content, isOwnMessage, user, id }) => (
+        <MessageCell
+          message={content}
+          isOwnMessage={isOwnMessage}
+          userName={user.name}
+          avatarUrl={user.avatarUrl}
+          key={id}
+          ref={ref => {
+            messageRefs.current[id] = ref;
+          }}
+        />
+      ))}
     </Container>
   );
 };
